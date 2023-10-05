5a86af0e8a18a5f00e087369befd58d3c70e75c0
Александр Корсунов <kors21vek@mail.ru>
2023-10-06 02:55:51 +0300

    fix style code

--- construction_obj/forms.py
+++ construction_obj/forms.py
@@ -3,10 +3,11 @@ from django.contrib.auth.forms import forms
 
 class DiscountForms(forms.Form):
     """Форма для ввода скидки."""
+
     discount = forms.DecimalField(
-        label='Скидка',
+        label="Скидка",
         min_value=0,
         max_value=100,
         decimal_places=2,
-        help_text='Введите размер скидки, % (от 0 до 100):'
+        help_text="Введите размер скидки, % (от 0 до 100):",
     )
--- construction_obj/urls.py
+++ construction_obj/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
-from construction_obj.views import ListBuildingView, DetailBuildingView, UpdateSectionView
+from construction_obj.views import (
+    ListBuildingView,
+    DetailBuildingView,
+    UpdateSectionView,
+)
 
 app_name = "construction_obj"
 
@@ -8,5 +12,4 @@ urlpatterns = [
     path("", ListBuildingView.as_view(), name="list_buildings"),
     path("building/<int:pk>/", DetailBuildingView.as_view(), name="detail_building"),
     path("update_section/<int:pk>/", UpdateSectionView.as_view(), name="update_section"),
-
 ]
